64f06b2177be3ad9450105bf79a41990ca163b5d
m-kordi <mh.alkordi@gmail.com>
2023-12-31 12:26:29 +0300

    done

--- src/components/homec/adop/Adop.jsx
+++ src/components/homec/adop/Adop.jsx
@@ -44,7 +44,7 @@ function Adop() {
           transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
         }}>
           <label htmlFor="search"><FcSearch className='sea-icon'/></label>
-          <input id='search' type="search" placeholder='مثال: قطة' onChange={search} autoComplete='off'/>
+          <input id='search' type="search" placeholder='مثال: قطة أو حلب' onChange={search} autoComplete='off'/>
         </div>
 
         <div className='adop-container'>
